206de23b46c6c8309489941136bafd97f0daff96
mavis-martins <mavismr98@gmail.com>
2023-11-20 23:54:33 -0300

    Atividade - Saída de Dados em Java.

---
+++ src/Atividade1.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+
+public class Atividade1 {
+
+	public static void main(String[] args) {
+		String product1 = "Computer";
+		String product2 = "Office Desk";
+		
+		int age = 30;
+		int code = 5290;
+		char gender = 'F';
+		
+		double price1 = 2100.0;
+		double price2 = 650.50;
+		double measure = 53.234567;
+		
+		System.out.println("Products:");
+		System.out.printf("%s, wich price is $ %.2f %n", product1, price1);
+		System.out.printf("%s, wich price is $ %.2f %n%n", product2, price2);
+		System.out.printf("Record: %d years old, code %d and gender: %s %n%n", age, code, gender);
+		System.out.printf("Measuse with eight decimal places: %.8f %n", measure);
+		System.out.printf("Rounded (three decimals places): %.3f %n", measure);
+		Locale.setDefault(Locale.US);
+		System.out.printf("US Decimal point: %.3f", measure);
+
+	}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Locale;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		
+		
+		double x = 10.580;
+		
+		System.out.println("Olá, mundo!");
+		System.out.printf("%.2f%n", x);
+
+	}
+
+}
